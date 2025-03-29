545824a474b62902eb3f543c8a6bc561eb75f5e4
Hemant Tiwari <hemant.1751@gmail.com>
2025-03-30 04:03:55 +0530

    pushing database schema

---
+++ database/schema.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE mortgage_db;
+
+USE mortgage_db;
+
+CREATE TABLE mortgages (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    credit_score INT NOT NULL,
+    loan_amount FLOAT NOT NULL,
+    property_value FLOAT NOT NULL,
+    annual_income FLOAT NOT NULL,
+    debt_amount FLOAT NOT NULL,
+    loan_type VARCHAR(50) NOT NULL,
+    property_type VARCHAR(50) NOT NULL
+);
